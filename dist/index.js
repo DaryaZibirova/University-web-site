28ad1d67ed901eb0ad4ad2b71e75c4436964cfb4
Darya Zibirova <83375983+DaryaZibirova@users.noreply.github.com>
2023-02-09 19:30:48 +0300

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,39 @@
+//Slider
+var sliderLine = document.querySelector('.slider-block__slider-line');
+var dots = document.querySelectorAll('.slider-block__dot');
+var position = 0;
+var dotIndex = 0;
+var prevSlide = function () {
+    if (position > 0) {
+        position -= 449;
+        dotIndex--;
+    }
+    else {
+        position = (dots.length - 1) * 449;
+        dotIndex = (dots.length - 1);
+    }
+    ;
+    sliderLine.style.left = -position + 'px';
+    thisSlide(dotIndex);
+    clearInterval(timer);
+    timer = setInterval(prevSlide, 2500);
+};
+var thisSlide = function (index) {
+    for (var _i = 0, dots_1 = dots; _i < dots_1.length; _i++) {
+        var dot = dots_1[_i];
+        dot.classList.remove('slider-block__dot_active');
+    }
+    ;
+    dots[index].classList.add('slider-block__dot_active');
+};
+dots.forEach(function (dot, index) {
+    dot.addEventListener('click', function () {
+        position = 449 * index;
+        sliderLine.style.left = -position + 'px';
+        dotIndex = index;
+        thisSlide(dotIndex);
+        clearInterval(timer);
+        timer = setInterval(prevSlide, 2500);
+    });
+});
+var timer = setInterval(prevSlide, 2500);
